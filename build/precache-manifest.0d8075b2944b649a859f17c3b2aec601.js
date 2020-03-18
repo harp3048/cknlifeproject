1a931e5b950ef53507582342039a63957cd67ddc
hps007 <hps007@hotmail.com>
2020-03-18 15:06:59 -0500

    removed build folder from gitignore

---
+++ build/precache-manifest.0d8075b2944b649a859f17c3b2aec601.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28346c301e3653fd974b7deb19b7b63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b9f8232fc896efb5888",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8fca67563eb51d824712",
+    "url": "/static/js/2.e0f84895.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e0f84895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9f8232fc896efb5888",
+    "url": "/static/js/main.57040cc4.chunk.js"
+  },
+  {
+    "revision": "f6f43096ed92fa146864",
+    "url": "/static/js/runtime-main.f277f35b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
