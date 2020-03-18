1a7a2d387090d6e7d24f3f6ce4429d77257f0892
hps007 <hps007@hotmail.com>
2020-03-18 14:48:21 -0500

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CKN life project
+          CKN life projects
         </a>
       </header>
     </div>
